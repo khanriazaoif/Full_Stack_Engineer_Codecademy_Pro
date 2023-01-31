7d24be33f9035d7b31665ec69540fcceccdf4af9
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-31 12:22:35 -0600

    Bracket Notation

--- Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
+++ Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
@@ -3,14 +3,27 @@
 //     color: 'silver'
 // };
 
+// let spaceship = {
+//     homePlanet: 'Earth',
+//     color: 'silver',
+//     'Fuel Type': 'Turbo Fuel',
+//     numCrew: 5,
+//     flightPath: ['Venus', 'Mars', 'Saturn']
+// };
+//
+// // Write your code below
+// let crewCount = spaceship.numCrew;
+// let planetArray = spaceship.flightPath;
+
 let spaceship = {
-    homePlanet: 'Earth',
-    color: 'silver',
-    'Fuel Type': 'Turbo Fuel',
-    numCrew: 5,
-    flightPath: ['Venus', 'Mars', 'Saturn']
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth',
+    numCrew: 5
 };
 
+let propName =  'Active Mission';
+
 // Write your code below
-let crewCount = spaceship.numCrew;
-let planetArray = spaceship.flightPath;
+let isActive = spaceship['Active Mission'];
+console.log(spaceship['Active Mission']);
