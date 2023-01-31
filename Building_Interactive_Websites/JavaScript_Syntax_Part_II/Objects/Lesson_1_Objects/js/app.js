0fcb0852f6bfc128516ca24f1a5141a87adfd6f7
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-31 16:00:38 -0600

    Pass by Reference

--- Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
+++ Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
@@ -55,39 +55,57 @@
 // alienShip.retreat();
 // alienShip.takeOff();
 
+// let spaceship = {
+//     passengers: null,
+//     telescope: {
+//         yearBuilt: 2018,
+//         model: "91031-XLT",
+//         focalLength: 2032
+//     },
+//     crew: {
+//         captain: {
+//             name: 'Sandra',
+//             degree: 'Computer Engineering',
+//             encourageTeam() { console.log('We got this!') },
+//             'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+//     },
+//     engine: {
+//         model: "Nimbus2000"
+//     },
+//     nanoelectronics: {
+//         computer: {
+//             terabytes: 100,
+//             monitors: "HD"
+//         },
+//         'back-up': {
+//             battery: "Lithium",
+//             terabytes: 50
+//         }
+//     }
+// };
+//
+//
+// let capFave = spaceship.crew.captain['favorite foods'][0];
+// spaceship.passengers = [{
+//     passenger: 'Riaz'
+// }];
+//
+// let firstPassenger = spaceship.passengers[0];
+
 let spaceship = {
-    passengers: null,
-    telescope: {
-        yearBuilt: 2018,
-        model: "91031-XLT",
-        focalLength: 2032
-    },
-    crew: {
-        captain: {
-            name: 'Sandra',
-            degree: 'Computer Engineering',
-            encourageTeam() { console.log('We got this!') },
-            'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
-    },
-    engine: {
-        model: "Nimbus2000"
-    },
-    nanoelectronics: {
-        computer: {
-            terabytes: 100,
-            monitors: "HD"
-        },
-        'back-up': {
-            battery: "Lithium",
-            terabytes: 50
-        }
-    }
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
 };
 
+// Write your code below
+function greenEnergy(spaceship){
+    spaceship['Fuel Type'] = 'avocado oil';
+}
 
-let capFave = spaceship.crew.captain['favorite foods'][0];
-spaceship.passengers = [{
-    passenger: 'Riaz'
-}];
+function remotelyDisable(spaceship){
+    spaceship.disabled = true;
+}
 
-let firstPassenger = spaceship.passengers[0];
+greenEnergy(spaceship);
+remotelyDisable(spaceship);
+console.log(spaceship);
