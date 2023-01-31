686379ccdce642e6d31a3836cd844fd8ef2c3c44
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-31 13:05:10 -0600

    Methods

--- Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
+++ Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
@@ -28,14 +28,29 @@
 // let isActive = spaceship['Active Mission'];
 // console.log(spaceship['Active Mission']);
 
-let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    homePlanet : 'Earth',
-    color: 'silver',
-    'Secret Mission' : 'Discover life outside of Earth.'
-};
+// let spaceship = {
+//     'Fuel Type' : 'Turbo Fuel',
+//     homePlanet : 'Earth',
+//     color: 'silver',
+//     'Secret Mission' : 'Discover life outside of Earth.'
+// };
+//
+// // Write your code below
+// spaceship.color = 'glorious gold';
+// spaceship.numEngines = 7;
+// delete spaceship['Secret Mission'];
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
 
 // Write your code below
-spaceship.color = 'glorious gold';
-spaceship.numEngines = 7;
-delete spaceship['Secret Mission'];
+alienShip = {
+    retreat () {
+        console.log(retreatMessage);
+    },
+    takeOff() {
+        console.log('Spim... Borp... Glix... Blastoff!');
+    }
+}
+
+alienShip.retreat();
+alienShip.takeOff();
