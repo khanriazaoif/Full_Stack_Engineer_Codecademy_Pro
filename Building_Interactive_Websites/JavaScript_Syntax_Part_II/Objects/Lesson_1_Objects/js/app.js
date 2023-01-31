2f934e5fdc09c9323bd617fb1bf9458e98df080b
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-31 16:37:40 -0600

    Looping Through Objects

--- Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
+++ Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
@@ -92,20 +92,53 @@
 //
 // let firstPassenger = spaceship.passengers[0];
 
+// let spaceship = {
+//     'Fuel Type' : 'Turbo Fuel',
+//     homePlanet : 'Earth'
+// };
+//
+// // Write your code below
+// function greenEnergy(spaceship){
+//     spaceship['Fuel Type'] = 'avocado oil';
+// }
+//
+// function remotelyDisable(spaceship){
+//     spaceship.disabled = true;
+// }
+//
+// greenEnergy(spaceship);
+// remotelyDisable(spaceship);
+// console.log(spaceship);
+
 let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    homePlanet : 'Earth'
+    crew: {
+        captain: {
+            name: 'Lily',
+            degree: 'Computer Engineering',
+            cheerTeam() { console.log('You got this!') }
+        },
+        'chief officer': {
+            name: 'Dan',
+            degree: 'Aerospace Engineering',
+            agree() { console.log('I agree, captain!') }
+        },
+        medic: {l
+            name: 'Clementine',
+            degree: 'Physics',
+            announce() { console.log(`Jets on!`) } },
+        translator: {
+            name: 'Shauna',
+            degree: 'Conservation Science',
+            powerFuel() { console.log('The tank is full!') }
+        }
+    }
 };
 
 // Write your code below
-function greenEnergy(spaceship){
-    spaceship['Fuel Type'] = 'avocado oil';
-}
+for (let crewMember in spaceship.crew){
+    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+};
 
-function remotelyDisable(spaceship){
-    spaceship.disabled = true;
+for (let crewMember in spaceship.crew) {
+    console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`);
 }
-
-greenEnergy(spaceship);
-remotelyDisable(spaceship);
-console.log(spaceship);
