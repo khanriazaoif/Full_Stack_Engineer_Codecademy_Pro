37b887f87b48910cccc3d470bc28e22ccd099c85
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-31 15:41:47 -0600

    Nested Objects

--- Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
+++ Building_Interactive_Websites/JavaScript_Syntax_Part_II/Objects/Lesson_1_Objects/js/app.js
@@ -40,17 +40,54 @@
 // spaceship.numEngines = 7;
 // delete spaceship['Secret Mission'];
 
-let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+// let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+//
+// // Write your code below
+// alienShip = {
+//     retreat () {
+//         console.log(retreatMessage);
+//     },
+//     takeOff() {
+//         console.log('Spim... Borp... Glix... Blastoff!');
+//     }
+// }
+//
+// alienShip.retreat();
+// alienShip.takeOff();
 
-// Write your code below
-alienShip = {
-    retreat () {
-        console.log(retreatMessage);
+let spaceship = {
+    passengers: null,
+    telescope: {
+        yearBuilt: 2018,
+        model: "91031-XLT",
+        focalLength: 2032
     },
-    takeOff() {
-        console.log('Spim... Borp... Glix... Blastoff!');
+    crew: {
+        captain: {
+            name: 'Sandra',
+            degree: 'Computer Engineering',
+            encourageTeam() { console.log('We got this!') },
+            'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+        model: "Nimbus2000"
+    },
+    nanoelectronics: {
+        computer: {
+            terabytes: 100,
+            monitors: "HD"
+        },
+        'back-up': {
+            battery: "Lithium",
+            terabytes: 50
+        }
     }
-}
+};
 
-alienShip.retreat();
-alienShip.takeOff();
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+spaceship.passengers = [{
+    passenger: 'Riaz'
+}];
+
+let firstPassenger = spaceship.passengers[0];
